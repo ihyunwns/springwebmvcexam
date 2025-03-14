5a3ea216abd7bb30c167bfd7ba8d43bf2affe3fd
ihyunwns <ihyunwns@gmail.com>
2025-03-14 13:20:41 +0900

    progress-bar 수정 중

--- src/main/webapp/static/js/animal/manage.js
+++ src/main/webapp/static/js/animal/manage.js
@@ -6,8 +6,13 @@ const csrfHeader = document.querySelector('meta[name="_csrf_header"]').content;
 const socket = new WebSocket("ws://localhost:8080/ws-progress")
 const stompClient = Stomp.over(socket);
 
+const progressBar = document.querySelector(".progress-bar");
+const progressFill = document.querySelector(".progress-bar-fill");
+
 syncBtn.style.width = `${syncBtn.offsetWidth}px`;
 syncBtn.style.height = `${syncBtn.offsetHeight}px`;
+progressBar.style.width = `${syncBtn.offsetWidth}px`;
+progressBar.style.height = `${syncBtn.offsetHeight}px`;
 
 stompClient.connect({}, function() {
 
@@ -16,8 +21,11 @@ stompClient.connect({}, function() {
         const data = JSON.parse(message.body);
         const { keyword, progress } = data;
 
-        syncBtn.textContent = keyword;
-        animateProgress(progress);
+        syncBtn.style.display = "none";
+        progressBar.style.display = "block";
+        progressBar.textContent = keyword;
+
+        progressFill.style.width = `${progress * 100}%`
 
         console.log("진행 상황:", message.body);
     });
@@ -25,9 +33,7 @@ stompClient.connect({}, function() {
 
 syncBtn.addEventListener("click", () => {
 
-    syncBtn.disabled = true;
     syncBtn.innerHTML = `<span class="loading-spinner"></span>`;
-
     fetch('/animal/syncData', {
         method: "POST",
         headers:
@@ -38,27 +44,10 @@ syncBtn.addEventListener("click", () => {
         body: {}
         }).then( response => {
             if( response.ok ){
-                syncBtn.disabled = false;
-                syncBtn.innerHTML = '';
-                syncBtn.textContent = '데이터 갱신';
-                syncBtn.style.color = "#fff";
+                progressBar.style.display = "none";
+                syncBtn.style.display = "block";
 
                 console.log(" 동기화 작업 완료 ");
             }
         })
 })
-
-/* 버튼 방식하지 말고 div 방식으로 변경하자 ㅇㅇ */
-function animateProgress(target) {
-    let currentProgress = 0.0;
-
-    if (Math.abs(currentProgress - target) < 0.01) return; // 너무 미세한 차이는 무시
-    currentProgress += (target - currentProgress) * 0.1; // 부드러운 보간 (lerp)
-
-    let progressPercent = (currentProgress * 100).toFixed(2);
-
-    syncBtn.style.background = `linear-gradient(90deg, #3f51b5 ${progressPercent}%, #f9f9f9 ${progressPercent}%)`;
-    syncBtn.style.color = '#888888'
-
-    requestAnimationFrame(() => animateProgress(target)); // 부드럽게 이어짐
-}
